92bdf3601fa0837992ea8c59470999b059db08d6
Oziel Gonzalez <iskartours@gmail.com>
2025-09-05 21:05:25 -0500

    utlimo commit del dia

--- app/Views/pages/home.php
+++ app/Views/pages/home.php
@@ -1,1 +1,4 @@
 <?= $this->extend('templates/template')?>
+<?= $this->section('banner')?>
+<img src="./assets/images/banner1.webp" alt="">
+<?= $this->endSection()?>
--- app/Views/templates/template.php
+++ app/Views/templates/template.php
@@ -7,23 +7,31 @@
     
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="./assets/css/iskarstyle.css">
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
     
 </head>
 <body class="d-flex flex-column vh-100">
     <header>
         <div class="container-fluid">
-            <nav class="navbar bg-body-tertiary">
-                <div class="container-fluid">
+            <nav class="navbar fixed-top">
+                <div class="container">
                     <a class="navbar-brand" href="#">
-                        <img src="./assets/images/iskartourslogo.png" alt="Iskar Tours" width="125" height="35">
+                        <img src="./assets/images/iskartourslogo.png" alt="Iskar Tours" width="150" height="40">
                     </a>
                 </div>
             </nav>
         </div>
-        
+        <?= $this->renderSection('banner') ?>
     </header>
     <main></main>
-    <footer></footer>
+    <footer class="mt-auto">
+        <div class="container-fluid">
+            <div class="container">
+                Iskar Tours &copy;2025. Derechos Reservados
+            </div>
+        </div>
+        
+    </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
