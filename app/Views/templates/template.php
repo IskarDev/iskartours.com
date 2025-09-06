4d00631bbd1334a62c44290e0daeedd02c3d90a2
Oziel Gonzalez <iskartours@gmail.com>
2025-09-05 20:10:13 -0500

    primeros cambios reales

--- app/Views/templates/template.php
+++ app/Views/templates/template.php
@@ -3,12 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Iskar Tours Web Page</title>
+    
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="./assets/css/iskarstyle.css">
+    
 </head>
-<body>
-    hola mundo
+<body class="d-flex flex-column vh-100">
+    <header>
+        <div class="container-fluid">
+            <nav class="navbar bg-body-tertiary">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">
+                        <img src="./assets/images/iskartourslogo.png" alt="Iskar Tours" width="125" height="35">
+                    </a>
+                </div>
+            </nav>
+        </div>
+        
+    </header>
+    <main></main>
+    <footer></footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
