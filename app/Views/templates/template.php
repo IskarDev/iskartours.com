2df399ab74609d852ee6fa56da7c3a824d2e8dc4
Oziel Gonzalez <iskartours@gmail.com>
2025-09-11 15:13:00 -0500

    arreglando el footer

--- app/Views/templates/template.php
+++ app/Views/templates/template.php
@@ -28,20 +28,16 @@
             <?= $this->renderSection('banner') ?>
         </div>
     </header>
-    <hr>
     <main>
         <div class="container my-1 my-md-4">
             <?= $this->renderSection('content') ?>
         </div>
     </main>
-    <hr>
-    <footer class="mt-auto">
-        <div class="container-fluid">
-            <div class="container">
-                <p>
-                    Iskar Tours &copy;2025. All rights reserved.
-                </p> 
-            </div>
+    <footer class="bg-dark py-4 mt-auto">
+        <div class="container text-center">
+            <p class="mb-0 text-light">
+                Iskar Tours &copy; <?= date('Y') ?> . All rights reserved.
+            </p>
         </div>
     </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
