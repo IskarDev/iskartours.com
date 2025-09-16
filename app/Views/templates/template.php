0baa6e8f197115601f79704effde1e86ef1e2071
Oziel Gonzalez <iskartours@gmail.com>
2025-09-16 11:14:12 -0500

    añadiendo enlaces al footer

--- app/Views/templates/template.php
+++ app/Views/templates/template.php
@@ -33,9 +33,34 @@
             <?= $this->renderSection('content') ?>
         </div>
     </main>
-    <footer class="footer-dark py-4 mt-auto">
-        <div class="container text-center">
-            <p class="mb-0 text-light">
+    <footer class="footer-dark py-4 mt-auto text-light">
+        <div class="container text-center pt-5">
+            <div class="row">
+                
+                <div class="col-md-6">
+                    <div id="redesesociales">
+                        <h6>SOCIAL NETWORKS</h6>
+                        <p class="my-4">    
+                            <a class="personallinks" href="https://www.facebook.com/iskartours/" target="_blank"><i class="bi bi-facebook"></i></a>
+                            <a class="personallinks" href="https://www.instagram.com/iskartours/" target="_blank"><i class="bi bi-instagram"></i></a>
+                            <a class="personallinks" href="https://www.tiktok.com/@iskartours" target="_blank"><i class="bi bi-tiktok"></i></a>
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div id="redesesociales">
+                        <h6>OTHER LINKS</h6>
+                        <p>    
+                            <ul class="list-unstyled">
+                                <li><a class="personallinks" href="https://www.tckunayala.com/" target="_blank">Transporte y Carga Kuna Yala S.A.</a></li>
+                                <li><a class="personallinks" href="https://www.gunayala.org.pa/" target="_blank">Congreso General Guna</a></li>
+                                <li><a class="personallinks" href="https://www.atp.gob.pa/" target="_blank">Autoridad de Turismo de Panamá</a></li>
+                            </ul>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <p class="my-5">
                 Iskar Tours &copy; <?= date('Y') ?> . All rights reserved.
             </p>
         </div>
