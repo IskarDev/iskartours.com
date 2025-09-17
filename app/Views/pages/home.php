dbbdd31d05343c1717e194755652fab592d725e7
Oziel González <ozielgonzalez28583@gmail.com>
2025-09-16 19:14:56 -0500

    agregando seccion de trips and tour sin terminar

--- app/Views/pages/home.php
+++ app/Views/pages/home.php
@@ -10,9 +10,9 @@
     <div class="row">
         <div class="col col-md-6 col-lg-4">
             <div class="card">
-                <img src="./assets/images/aeropuerto.webp" alt="Aeropuerto">
+                <img src="./assets/images/aeropuerto.webp" alt="Airport Services">
                 <div class="card-body">
-                    <h5 class="card-title">
+                    <h5 class="card-title text-uppercase">
                         🚐 Private Transportation – Airport ↔ Hotel
                     </h5>
                     <p class="card-text">Start your trip in Panama with the comfort and safety of a private transfer from Tocumen International Airport to your hotel.</p>
@@ -26,11 +26,67 @@
         </div>
         <div class="col col-md-6 col-lg-4">
             <div class="card">
+                <img src="./assets/images/personal.webp" alt="Personalized Services">
+                <div class="card-body">
+                    <h5 class="card-title text-uppercase">
+                        🚐 Customized Transportation Services
+                    </h5>
+                    <p class="card-text">Enjoy the flexibility of private transportation tailored to your needs, perfect for beach trips, mountain getaways, events, or business travel anywhere in Panama.</p>
+                    <p class="card-text">Our professional and punctual driver will take you wherever you need to go, safely and comfortably.</p>
+                    <div class="d-grip gap-2">
+                        <a href="https://wa.me/50768434370" class="btn btn-success" role="button"><i class="bi bi-whatsapp"></i> Book Now</a>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+        <div class="col col-md-6 col-lg-4">
+            <div class="card">
+                <img src="./assets/images/corporativo.webp" alt="Corporative Services">
+                <div class="card-body">
+                    <h5 class="card-title text-uppercase">
+                        💼 Corporate Transfers
+                    </h5>
+                    <p class="card-text">We provide reliable executive transportation for business meetings, conferences, staff movements, or client visits.</p>
+                    <p class="card-text">Our professional and punctual driver ensures comfort, discretion, and efficiency for you and your team.</p>
+                    <div class="d-grip gap-2">
+                        <a href="https://wa.me/50768434370" class="btn btn-success" role="button"><i class="bi bi-whatsapp"></i> Book Now</a>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<hr class="my-1 my-md-5">
+<div id="tours">
+    <h2>Trips & Tours</h2>
+    <div class="row">
+        <div class="col col-md-6 col-lg-4">
+            <div class="card">
+                <img src="./assets/images/sanblas.webp" alt="San Blas Tours">
+                <div class="card-body">
+                    <h5 class="card-title">
+                        🌴 DAILY TOURS TO SAN BLAS
+                    </h5>
+                    <p class="card-text">Enjoy an unforgettable experience in the beautiful San Blas Islands, a Caribbean paradise in Panama with crystal-clear waters, white-sand beaches, and the vibrant Guna indigenous culture.</p>
+                    <p class="card-text">
+                        👉 Book your spot in advance and get ready to discover one of Panama’s most breathtaking destinations!
+                    </p>
+                    <div class="d-grip gap-2">
+                        <a href="https://wa.me/50768434370" class="btn btn-success" role="button"><i class="bi bi-whatsapp"></i> Book Now</a>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+        <div class="col col-md-6 col-lg-4">
+            <div class="card">
                 <img src="./assets/images/personal.webp" alt="Aeropuerto">
                 <div class="card-body">
                     <h5 class="card-title">
-                        🚐 Customized Transportation Services
-                    </h5>
+                       🌿 Gamboa Monkeys Island Tours
+                    </h5> 
                     <p class="card-text">Enjoy the flexibility of private transportation tailored to your needs, perfect for beach trips, mountain getaways, events, or business travel anywhere in Panama.</p>
                     <p class="card-text">Our professional and punctual driver will take you wherever you need to go, safely and comfortably.</p>
                     <div class="d-grip gap-2">
